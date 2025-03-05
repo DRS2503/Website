96618cde7ef9184f091f396882e9832564371098
DerekSchramm <drschramm33@gmail.com>
2025-03-05 08:53:00 -0500

    Added new divs to the main

--- script.js
+++ script.js
@@ -1,1 +1,1 @@
-document.write("This is a JS code");
+document.write("I am Derek Schramm a Computer Science Student at Oakland University. I am a senior working on my degree in Bachlor's in Computer Science");
